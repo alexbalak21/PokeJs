f7abe4c7c48fb95b3a46ba817d3f00b9a7a80901
alexblack24 <alex.black@mail.com>
2024-08-28 14:22:12 +0200

    Background Image drawn, player drawn

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ const canvas = document.querySelector("canvas")
 const c = canvas.getContext("2d")
 const img = new Image()
 img.src = "./img/Town.png"
-
+const playerImage = new Image()
+playerImage.src = "./img/playerDown.png"
 canvas.width = 1024
 canvas.height = 576
 
-img.onload = () => {
-  c.drawImage(img, -720, -600)
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.drawImage(img, -734, -600)
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  )
 }
+animate()
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "s":
+      console.log("down")
+      break
+    case "z":
+      console.log("up")
+      break
+    case "q":
+      console.log("left")
+      break
+    case "d":
+      console.log("right")
+      break
+  }
+})
