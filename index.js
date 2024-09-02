e4b2f3228c8e5ba290231d65757e40a246b254d5
alexblack24 <alex.black@mail.com>
2024-09-02 10:45:17 +0200

    collision detection added

--- index.js
+++ index.js
@@ -52,6 +52,10 @@ class Sprite {
     this.position = position
     this.image = image
     this.frames = frames
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max
+      this.height = this.image.height
+    }
   }
 
   draw() {
@@ -109,6 +113,15 @@ function animate() {
   // boundaries.forEach((boundary) => boundary.draw())
   testBoundary.draw()
   player.draw()
+
+  if (
+    player.position.x + player.width >= testBoundary.position.x &&
+    player.position.x <= testBoundary.position.x + testBoundary.width &&
+    player.position.y + player.height >= testBoundary.position.y &&
+    player.position.y <= testBoundary.position.y + testBoundary.height
+  ) {
+    console.log("Colliding")
+  }
 
   if (keys.down && lastKey === "down") {
     movables.forEach((movable) => (movable.position.y -= 3))
